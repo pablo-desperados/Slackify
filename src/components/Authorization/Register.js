8d754303ee8e9c04bf576e4eee31b84aee36bb9e
pablo mujica <pablom@pablos-MacBook-Pro.local>
2020-09-11 00:20:48 -0400

    Added error handling for register form

--- src/components/Authorization/Register.js
+++ src/components/Authorization/Register.js
@@ -5,15 +5,49 @@ class Register extends React.Component{
     constructor(props){
         super(props)
         this.state={
-            
+            username: "",
+            email: "",
+            password: "",
+            passwordConfirmation: "",
+            errors: ""
         }
         this.handleChange = this.handleChange.bind(this)
+        this.handleSubmit = this.handleSubmit.bind(this)
     }
 
-    handleChange(){
-
+    handleChange(event){
+        
+        let state = event.target.name
+        this.setState({[state]: event.target.value, errors:""})
     }
+
+    handleSubmit(event){
+        event.preventDefault();
+        let payload = {
+            username: this.state.username,
+            password: this.state.password,
+            passwordConfirmation: this.state.passwordConfirmation,
+            email: this.state.email,
+            error:[]
+          }
+          debugger
+          if (payload.username.trim().length < 1 || payload.password.trim().length < 1 || payload.email.trim().length<1){
+            this.setState({errors: "One or more fields are missing"})
+          }else if(payload.password.trim()!=payload.passwordConfirmation.trim()){
+            this.setState({errors: "The passwords you entered do not match"})
+          }else{
+              console.log("hello")
+          }
+    }
+
     render(){
+        let errordiv = ""
+        if (this.state.errors.trim().length > 1) {
+          errordiv = <Message negative>
+            <Message.Header>An Error Has Ocurred</Message.Header>
+            <p>{this.state.errors}</p>
+        </Message>
+        }
         return(
         <div>
              <Grid textAlign='center'  verticalAlign='middle' className="app">
@@ -22,18 +56,19 @@ class Register extends React.Component{
                         <Icon icon="cog"name="Slackify" color="green" />
                         Join Slackify!
                     </Header>
-                    <Form size="large">
+
+                    <Form size="large" onSubmit={this.handleSubmit}>
                         <Segment>
-                            <Form.Input fluid name="Username" icon="user outline" iconPosition="left" placeholder="username" onChange={this.handleChange} type="text"></Form.Input>
-                            <Form.Input fluid name="Email" icon="mail" iconPosition="left" placeholder="email" onChange={this.handleChange} type="text"></Form.Input>
-                            <Form.Input fluid name="Password" icon="lock" iconPosition="left" placeholder="password" onChange={this.handleChange} type="password"></Form.Input>
-                            <Form.Input fluid name="PasswordConfirmation" icon="unlock" iconPosition="left" placeholder="password confirmation" onChange={this.handleChange} type="password"></Form.Input>
+                            {errordiv}
+                            <Form.Input fluid name="username" icon="user outline" iconPosition="left" placeholder="username" onChange={this.handleChange} type="text"></Form.Input>
+                            <Form.Input fluid name="email" icon="mail" iconPosition="left" placeholder="email" onChange={this.handleChange} type="text"></Form.Input>
+                            <Form.Input fluid name="password" icon="lock" iconPosition="left" placeholder="password" onChange={this.handleChange} type="password"></Form.Input>
+                            <Form.Input fluid name="passwordConfirmation" icon="unlock" iconPosition="left" placeholder="password confirmation" onChange={this.handleChange} type="password"></Form.Input>
                             <Button content="Register Now!" color="green" fluid size="large"></Button>
                         </Segment>
                         <Message>Already a user? Log In! <Link to="/login"></Link></Message>
                     </Form>
                  </Grid.Column>
-
              </Grid>
         </div>
         )
