df53773da6e2d20627a663ee6cf4cf015f676e71
pablo mujica <pablom@pablos-MacBook-Pro.local>
2020-09-10 23:32:05 -0400

    Added registration page (non-working)

--- src/components/Authorization/Register.js
+++ src/components/Authorization/Register.js
@@ -1,18 +1,40 @@
 import React from "react"
-
+import {Grid, Form, Button, Segment, Header, Message, Icon, GridColumn} from "semantic-ui-react"
+import{Link} from 'react-router-dom'
 class Register extends React.Component{
     constructor(props){
         super(props)
         this.state={
-
+            
         }
+        this.handleChange = this.handleChange.bind(this)
     }
 
+    handleChange(){
+
+    }
     render(){
         return(
         <div>
-            Register
+             <Grid textAlign='center'  verticalAlign='middle' className="app">
+                 <Grid.Column style={{maxWidth:450}}>
+                    <Header as="h2" icon color="green" textAlign="center">
+                        <Icon icon="cog"name="Slackify" color="green" />
+                        Join Slackify!
+                    </Header>
+                    <Form size="large">
+                        <Segment>
+                            <Form.Input fluid name="Username" icon="user outline" iconPosition="left" placeholder="username" onChange={this.handleChange} type="text"></Form.Input>
+                            <Form.Input fluid name="Email" icon="mail" iconPosition="left" placeholder="email" onChange={this.handleChange} type="text"></Form.Input>
+                            <Form.Input fluid name="Password" icon="lock" iconPosition="left" placeholder="password" onChange={this.handleChange} type="password"></Form.Input>
+                            <Form.Input fluid name="PasswordConfirmation" icon="unlock" iconPosition="left" placeholder="password confirmation" onChange={this.handleChange} type="password"></Form.Input>
+                            <Button content="Register Now!" color="green" fluid size="large"></Button>
+                        </Segment>
+                        <Message>Already a user? Log In! <Link to="/login"></Link></Message>
+                    </Form>
+                 </Grid.Column>
 
+             </Grid>
         </div>
         )
     }
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './components/App.js';
 import registerServiceWorker from './registerServiceWorker';
 import Login from './components/Authorization/Login.js'
 import Register from './components/Authorization/Register.js'
 import {BrowserRouter, Switch, Route} from "react-router-dom"
+import "semantic-ui-css/semantic.min.css"
 
 const  Root = () =>(
     <BrowserRouter>
